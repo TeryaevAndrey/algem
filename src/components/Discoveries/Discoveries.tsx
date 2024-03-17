e1cb8e05f7731669b8d5cea16954a34867f7a4b9
Andrey <teryaev.2004@mail.ru>
2024-03-17 21:48:58 +0300

    Added animation for Discoveries section

--- src/components/Discoveries/Discoveries.tsx
+++ src/components/Discoveries/Discoveries.tsx
@@ -1,21 +1,37 @@
 "use client";
 
-import { useScroll, useTransform, motion } from "framer-motion";
+import { useScroll, useTransform, motion, useSpring } from "framer-motion";
 import styles from "./Discoveries.module.css";
+import { useEffect, useRef } from "react";
 
 export const Discoveries = () => {
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1.05, 1]);
+  const ref = useRef<HTMLDivElement>(null);
 
-  console.log(scale);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const scale = useTransform(scrollYProgress, [0.2, 1], [0.2, 1]);
+  const borderRadius = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["15rem", "0.8rem"]
+  );
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
 
+  const springScale = useSpring(scale, { stiffness: 100, damping: 30 });
+  
   return (
-    <motion.div className="px-4 pt-16 lg:pt-20 bg-white" style={{ scale }}>
-      <div className={styles.bg}>
+    <motion.div ref={ref} className="px-4 pt-16 lg:pt-20 bg-white h-[250vh]">
+      <motion.div
+        className={styles.bg}
+        style={{
+          borderRadius,
+          opacity,
+          scale: springScale,
+        }}
+      >
         <h2 className="lg:max-w-[1000px] px-8">
           Discoveries beyond the horizon
         </h2>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
