547adbad52545d8af34309cac9c52b88ce82d652
Andrey <teryaev.2004@mail.ru>
2024-03-20 11:32:14 +0300

    Fix

--- src/components/AnimationBlocks/AnimationBlocks.tsx
+++ src/components/AnimationBlocks/AnimationBlocks.tsx
@@ -7,136 +7,147 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 export const AnimationBlocks = () => {
   const ref = useRef<HTMLDivElement>(null);
+  const ref2 = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({ target: ref });
+  const { scrollYProgress: scrollYProgress2 } = useScroll({ target: ref2 });
 
-  const opacity1 = useTransform(scrollYProgress, [0.02, 0.2], [1, 0]);
-  const opacity2 = useTransform(scrollYProgress, [0.22, 0.4], [1, 0]);
-  const opacity3 = useTransform(scrollYProgress, [0.42, 0.6], [1, 0]);
-  const opacity4 = useTransform(scrollYProgress, [0.62, 0.8], [1, 0]);
-  const opacity5 = useTransform(scrollYProgress, [0.82, 1], [1, 0]);
+  const opacity1 = useTransform(scrollYProgress, [0.02, 0.5], [1, 0]);
+  const opacity2 = useTransform(scrollYProgress, [0.55, 1], [1, 0]);
+  // const opacity3 = useTransform(scrollYProgress, [0.68, 1], [1, 0]);
+  const opacity4 = useTransform(scrollYProgress2, [0.02, 0.5], [1, 0]);
+  const opacity5 = useTransform(scrollYProgress2, [0.55, 1], [1, 0]);
 
   const translateY2 = useTransform(
     scrollYProgress,
-    [0.02, 0.2],
+    [0.02, 0.5],
     ["0vh", "-100vh"]
   );
 
   const translateY3 = useTransform(
     scrollYProgress,
-    [0.22, 0.4],
-    ["0vh", "-100vh"]
-  );
-
-  const translateY4 = useTransform(
-    scrollYProgress,
-    [0.42, 0.6],
+    [0.55, 1],
     ["0vh", "-100vh"]
   );
 
   const translateY5 = useTransform(
-    scrollYProgress,
-    [0.62, 0.8],
+    scrollYProgress2,
+    [0.05, 0.5],
     ["0vh", "-100vh"]
   );
 
   const translateY6 = useTransform(
-    scrollYProgress,
-    [0.82, 1],
+    scrollYProgress2,
+    [0.55, 1],
     ["0vh", "-100vh"]
   );
 
   return (
     <div className="py-6 lg:py-[120px]">
-      <div
-        className="hidden lg:flex flex-col gap-12 lg:gap-0 h-[600vh] sticky top-0 "
-        ref={ref}
-      >
-        <div className="h-screen sticky top-0">
-          <motion.div
-            className="ease-linear duration-500"
-            style={{ opacity: opacity1 }}
-          >
-            <AnimationBlock
-              title="Supply your tokens to the pools on the partner DEXs"
-              suptitle="The Liquid Farming"
-              img="/images/animation-block-1.jpg"
-            />
-          </motion.div>
+      <div>
+        <div
+          className="hidden lg:flex flex-col gap-12 lg:gap-0 h-[300vh] sticky top-0 "
+          ref={ref}
+        >
+          <div className="h-screen sticky top-0">
+            <motion.div
+              className="ease-linear duration-500"
+              style={{ opacity: opacity1 }}
+            >
+              <AnimationBlock
+                title="Supply your tokens to the pools on the partner DEXs"
+                suptitle="The Liquid Farming"
+                img="/images/animation-block-1.jpg"
+              />
+            </motion.div>
 
-          <motion.div
-            className="absolute ease-out duration-500"
-            style={{ translateY: translateY2, opacity: opacity2 }}
-          >
-            <AnimationBlock
-              className="snap-start"
-              title="Get liquid lASTR tokens as the equivalent of your position"
-              suptitle="The Liquid Farming"
-              img="/images/animation-block-2.jpg"
-            />
-          </motion.div>
+            <motion.div
+              className="absolute ease-out duration-500"
+              style={{ translateY: translateY2, opacity: opacity2 }}
+            >
+              <AnimationBlock
+                className="snap-start"
+                title="Get liquid lASTR tokens as the equivalent of your position"
+                suptitle="The Liquid Farming"
+                img="/images/animation-block-2.jpg"
+              />
+            </motion.div>
 
-          <motion.div
-            className="absolute ease-out duration-500"
-            style={{ translateY: translateY3, opacity: opacity3 }}
-          >
-            <AnimationBlock
-              title="Earn triple rewards in the form of the ALGM incentives, DEXs and l-tokens farming rewards"
-              suptitle="The Liquid Farming"
-              img="/images/animation-block-3.jpg"
-              btn={
-                <Button
-                  className="!bg-base-content-primary !h-[56px] mt-3 lg:mt-0"
-                  title="Coming soon"
-                  size="big"
-                  type="secondary"
-                />
-              }
-            />
-          </motion.div>
+            <motion.div
+              className="absolute ease-out duration-500"
+              style={{ translateY: translateY3 }}
+            >
+              <AnimationBlock
+                title="Earn triple rewards in the form of the ALGM incentives, DEXs and l-tokens farming rewards"
+                suptitle="The Liquid Farming"
+                img="/images/animation-block-3.jpg"
+                btn={
+                  <Button
+                    className="!bg-base-content-primary !h-[56px] mt-3 lg:mt-0"
+                    title="Coming soon"
+                    size="big"
+                    type="secondary"
+                  />
+                }
+              />
+            </motion.div>
+          </div>
+        </div>
+      </div>
 
-          <motion.div
-            className="absolute ease-out duration-500"
-            style={{ translateY: translateY4, opacity: opacity4 }}
-          >
-            <AnimationBlock
-              title="Stake ASTR "
-              suptitle="The Liquid Farming"
-              img="/images/animation-block-4.jpg"
-              reverse={true}
-            />
-          </motion.div>
+      <div className="hidden lg:block">
+        <div className="bg-white h-screen"></div>
+      </div>
 
-          <motion.div
-            className="absolute ease-out duration-500"
-            style={{ translateY: translateY5, opacity: opacity5 }}
-          >
-            <AnimationBlock
-              title="Get liquid nASTR tokens and start to receive staking rewards"
-              suptitle="The Liquid Farming"
-              img="/images/animation-block-5.jpg"
-              reverse={true}
-            />
-          </motion.div>
+      <div>
+        <div
+          className="hidden lg:flex flex-col gap-12 lg:gap-0 h-[300vh] sticky top-0 "
+          ref={ref2}
+        >
+          <div className="h-screen sticky top-0">
+            <motion.div
+              className="ease-out duration-500"
+              style={{ opacity: opacity4 }}
+            >
+              <AnimationBlock
+                title="Stake ASTR "
+                suptitle="The Liquid Farming"
+                img="/images/animation-block-4.jpg"
+                reverse={true}
+              />
+            </motion.div>
 
-          <motion.div
-            className="absolute ease-out duration-500"
-            style={{ translateY: translateY6 }}
-          >
-            <AnimationBlock
-              title="Use your nASTR tokens across DeFi to increase your profits"
-              suptitle="The Liquid Farming"
-              img="/images/animation-block-6.jpg"
-              reverse={true}
-              btn={
-                <Button
-                  className="!bg-base-content-primary !h-[56px] mt-3 lg:mt-0"
-                  title="Stake now"
-                  size="big"
-                  type="secondary"
-                />
-              }
-            />
-          </motion.div>
+            <motion.div
+              className="absolute ease-out duration-500"
+              style={{ translateY: translateY5, opacity: opacity5 }}
+            >
+              <AnimationBlock
+                title="Get liquid nASTR tokens and start to receive staking rewards"
+                suptitle="The Liquid Farming"
+                img="/images/animation-block-5.jpg"
+                reverse={true}
+              />
+            </motion.div>
+
+            <motion.div
+              className="absolute ease-out duration-500"
+              style={{ translateY: translateY6 }}
+            >
+              <AnimationBlock
+                title="Use your nASTR tokens across DeFi to increase your profits"
+                suptitle="The Liquid Farming"
+                img="/images/animation-block-6.jpg"
+                reverse={true}
+                btn={
+                  <Button
+                    className="!bg-base-content-primary !h-[56px] mt-3 lg:mt-0"
+                    title="Stake now"
+                    size="big"
+                    type="secondary"
+                  />
+                }
+              />
+            </motion.div>
+          </div>
         </div>
       </div>
 
